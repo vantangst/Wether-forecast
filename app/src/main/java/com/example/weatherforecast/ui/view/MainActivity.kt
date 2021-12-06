2b1c4c93f2e68968ef6dbdd875802e46af7ae573
tangle <tang.le@liv3ly.io>
2021-12-06 14:33:33 +0700

    init project

---
+++ app/src/main/java/com/example/weatherforecast/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weatherforecast.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherforecast.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
